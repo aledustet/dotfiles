8060d51367b8d362c588a97a87f0bda6996a6e41
Alejandro Dustet <alejandro.dustetmederos@shopify.dev>
2022-03-25 16:38:55 -0400

    Moving existing files to avoid interactive rcup

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,16 @@ if [ ! -d $HOME/dotfiles-local ];then
   cp -r ./dotfiles-local $HOME
 fi
 
+if [ -f $HOME/.gitconfig ];then
+  mv $HOME/.gitconfig $HOME/gitconfig.old
+fi
+
 if ! command -v gst &> /dev/null; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ -f $HOME/.zshrc ];then
+  mv $HOME/.zshrc $HOME/zshrc.old
 fi
 
 env RCRC=$HOME/dotfiles/rcrc rcup
